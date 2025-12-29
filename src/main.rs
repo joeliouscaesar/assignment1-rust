108edf74e8a37b034663510711b6bdc6cdfe03fc
joeliouscaesar <joel.steinberg@gmail.com>
2025-12-29 18:09:31 -0500

    multiple processing pretokens but slow

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use fancy_regex::Regex;
-use std::fs;
+// use fancy_regex::Regex;
+use regex::bytes::Regex;
 use std::collections::{HashMap, BTreeSet};
 use std::hash::Hash;
 use std::vec;
+use memmap2::Mmap;
+use memchr::memmem;
+use rayon::prelude::*;
+use std::time::Instant;
 
 struct Pretoken {
     count:usize,
@@ -55,59 +59,80 @@ let x:Vec<u8> = vec![1,12,13];
 
 }
 
-fn get_pretoken_list() -> Vec<Pretoken>{
+fn regex_test<'a>(content:&'a [u8], re:&Regex, mut counts:HashMap<&'a [u8], usize>) -> HashMap<&'a [u8], usize>{
+    // let content = b"I'm the content, just a string. well bytes. whatever. hopefully this works. it's a long shot, no doubt.";
+    let byte_vecs = re.find_iter(content).map(|x| x.as_bytes());
+    for bv in byte_vecs{
+        *counts.entry(bv).or_insert(0) += 1;
+    }
+    return counts;
+}
+
+fn get_pretoken_list2() -> Option<Vec<Pretoken>> {
    // PRETOKENS
-    let mut pretoken_counts:HashMap<String, usize> = HashMap::new();
+   
     let mut pretoken_list:Vec<Pretoken> = Vec::new();
 
     // compile regex
-    let re = Regex::new(r"'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+");
+    // let re = Regex::new(r"'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+");
+    let re = Regex::new(r"'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+$|\s+");
     // pattern match on if error
     let re = match re {
         Ok(x) => x,
         Err(e) => {
             println!("regex issue {e}");
-            return pretoken_list;
+            return None;
         }
     };
 
-    // read in file
-    let contents = fs::read_to_string("data/corpus.en");
-    let contents = match contents {
-        Ok(s) => s,
-        Err(e) => {
-            println!("issue reading file: {e}");
-            return pretoken_list;
-        }
+    //read in with mmap
+    let fi = std::fs::File::open("data/TinyStoriesV2-GPT4-train.txt");
+    // let fi = std::fs::File::open("data/fake.txt");
+    let fi = match fi {
+        Err(_) => return None,
+        Ok(fi) => fi
     };
-    // add values to a hash map
-    let re_iter = re.find_iter(&contents);
-    for re_match in re_iter {
+    let mmap = unsafe { Mmap::map(&fi)};
+    let mmap = match mmap {
+        Err(_) => {
+            println!("mmap issue");
+            return None
+        },
+        Ok(mmm) => mmm
+    };
+    // get the delimiters
+    let delimiter = b"<|endoftext|>";
+    let delim_len = delimiter.len();
+    let finder = memmem::Finder::new(delimiter);
+    // return pairs of indices that are blocks to read in (b/n delimiters)
+    let mut chunks:Vec<(usize,usize)> = Vec::new();
+    let mut start:usize = 0;
+    for end in finder.find_iter(&mmap) {
+        if end != start {
+            chunks.push((start, end));
+        }
+        start = end + delim_len;
+    }
 
-        // string pretoken
-        let group = match re_match {
-            Ok(g) => g,
-            Err(e) => {
-                println!("issue with group {e}");
-                continue;
+    let pretoken_counts = chunks.par_iter().fold(|| HashMap::new(),
+        |current_counts, c:&(usize,usize)| {
+            regex_test(&mmap[c.0..c.1],&re,current_counts)
+    }).reduce(|| HashMap::new(), 
+        |mut mapa, mapb| {
+            for (k,v) in mapb {
+                *mapa.entry(k).or_insert(0) += v;
             }
-        };
-        let group = group.as_str().to_string();
+            return mapa
+    });
 
-        let count = match pretoken_counts.get(&group){
-            None => 0,
-            Some(n) => *n,
-        };
-        let count = count + 1;
-        pretoken_counts.insert(group, count);
-
-    } 
+    let pretoken_count_len = pretoken_counts.len();
+    println!("pretoken counts {pretoken_count_len}");
 
     for(pretoken_str, n) in pretoken_counts {
         // for pretoken objects
         let mut al = Vec::new();
-        for b in pretoken_str.bytes() {
-            al.push(vec![b]);
+        for b in pretoken_str {
+            al.push(vec![b.clone()]);
         }
         let new_pretoken = Pretoken{
             count: n,
@@ -115,8 +140,73 @@ fn get_pretoken_list() -> Vec<Pretoken>{
         };
         pretoken_list.push(new_pretoken);
     };
-    return pretoken_list;
+    return Some(pretoken_list);
 }
+
+
+// fn get_pretoken_list() -> Option<Vec<Pretoken>>{
+//    // PRETOKENS
+//     let mut pretoken_counts:HashMap<String, usize> = HashMap::new();
+//     let mut pretoken_list:Vec<Pretoken> = Vec::new();
+
+//     // compile regex
+//     let re = Regex::new(r"'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+");
+//     // pattern match on if error
+//     let re = match re {
+//         Ok(x) => x,
+//         Err(e) => {
+//             println!("regex issue {e}");
+//             return None;
+//         }
+//     };
+
+//     // read in file
+//     // let contents = fs::read_to_string("data/corpus.en");
+//     let contents = fs::read_to_string("data/TinyStoriesV2-GPT4-valid.txt");
+//     let contents = match contents {
+//         Ok(s) => s,
+//         Err(e) => {
+//             println!("issue reading file: {e}");
+//             return None;
+//         }
+//     };
+//     // add values to a hash map
+//     let re_iter = re.find_iter(&contents);
+//     for re_match in re_iter {
+
+//         // string pretoken
+//         let group = match re_match {
+//             Ok(g) => g,
+//             Err(e) => {
+//                 println!("issue with group {e}");
+//                 continue;
+//             }
+//         };
+//         let group = group.as_str().to_string();
+
+//         let count = match pretoken_counts.get(&group){
+//             None => 0,
+//             Some(n) => *n,
+//         };
+//         let count = count + 1;
+//         pretoken_counts.insert(group, count);
+
+//     } 
+
+//     for(pretoken_str, n) in pretoken_counts {
+//         // for pretoken objects
+//         let mut al = Vec::new();
+//         for b in pretoken_str.bytes() {
+//             al.push(vec![b]);
+//         }
+//         let new_pretoken = Pretoken{
+//             count: n,
+//             alphabet_list: al,
+//         };
+//         pretoken_list.push(new_pretoken);
+//     };
+//     return Some(pretoken_list);
+// }
 
 fn count_initial_pairs(pretoken_list:&Vec<Pretoken>) -> HashMap<AlphabetPair, AlphabetPairInfo>{
     let mut alphabet_pair_hash:HashMap<AlphabetPair,AlphabetPairInfo> = HashMap::new();
@@ -152,8 +242,6 @@ fn make_alphabet_pair_tree(initial_counts:&HashMap<AlphabetPair, AlphabetPairInf
     }
     return ap_tree;
 }
-
-
 fn new_alphabet_list(pretoken:&Pretoken, pair:&AlphabetPair) -> Vec<Vec<u8>>{
     let pair_tuple = &pair.pair;
     let mut i:usize = 1;
@@ -237,14 +325,22 @@ fn update_pairs_to_change(
 }
 
 fn train(num_merges:usize) -> Option<Vec<AlphabetPair>> {
+    let t_start = Instant::now();
+    println!("Starting training {t_start:?}");
+
     // for now just returns merges
     let mut merges:Vec<AlphabetPair> = Vec::new();
 
     // get list of pretokens
-    let mut pretoken_list = get_pretoken_list();
+    let mut pretoken_list = get_pretoken_list2()?;
     // print some summary stats
     let num_pretokens = pretoken_list.len();
     println!("total number of pretokens: {num_pretokens}");
+
+    let t_pretoke = Instant::now();
+    println!("Done pretokenizing {t_pretoke:?}");
+    let elapsed = t_pretoke - t_start;
+    println!("elapsed: {elapsed:?}");
 
     // get initial counts as a hash, then sort using a BTreeMap
     let mut ap_hash = count_initial_pairs(&pretoken_list);
@@ -278,19 +374,32 @@ fn train(num_merges:usize) -> Option<Vec<AlphabetPair>> {
         ap_tree.remove(&max_ap_clone);
     }
 
+    let t_merges = Instant::now();
+    println!("Done merging {t_merges:?}");
+    let elapsed = t_merges - t_pretoke;
+    println!("elapsed: {elapsed:?}");
+    let elapsed = t_merges - t_start;
+    println!("total train elapsed: {elapsed:?}");
+
     return Some(merges);
 }
 
 
 
 fn main() {
-    match train(10){
+    match train(10000){
         None => println!("no merges returned"),
         Some(merges) => {
-            println!("MERGES");
+
+            println!("MERGES (first 10)");
+            let mut i = 0;
             for merge in merges{
                 let pair = merge.pair;
-                println!("{pair:?}")
+                println!("{pair:?}");
+                i += 1;
+                if i > 10 {
+                    break;
+                }
             }
         }
     };
